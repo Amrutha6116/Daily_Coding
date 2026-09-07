e4d7350c3babc0a71fdce6d363ef23fed8ce5ffb
Amrutha KN <amruthakn6116@gmail.com>
2026-09-07 21:05:46 +0530

    Pattern Solid Square

---
+++ PatternSolidSquare.java
@@ -0,0 +1,15 @@
+public class PatternSolidSquare {
+    public static void main(String[] args) {
+        int n=7;
+        for(int i=1;i<=n;i++){
+            System.out.println("");
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+        }
+    }
+    
+}
+
+    
+
